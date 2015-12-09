0eed01bbbbf41dcf2f71f6de5e62387da19447cc
Kaihang An <khangan@thoughtworks.com>
2015-12-10 00:08:54 +0800

    finish Packing Boy CarIn feature.

---
+++ homework3/PackingLot.Test/PackingBoy.Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace PackingLot.Test
+{
+    public class PackingBoyTest
+    {
+        [Fact]
+        public void SuccessPackToFirstValidPackingLotTest()
+        {
+            var packingBoy = new PackingBoy(10);
+            var actual = packingBoy.PackingCar("xxx");
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void FailedPackingWhenAllPackingLotsAreFullTest()
+        {
+            var packingLots = new List<PackingLot>();
+            packingLots.Add(new PackingLot() { Limit = 1 });
+            var packingBoy = new PackingBoy(packingLots);
+            packingBoy.PackingCar("xxx");
+            var actual = packingBoy.PackingCar("ooo");
+            Assert.Null(actual);
+        }
+    }
+}
---
+++ homework3/PackingLot/PackingBoy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingLot
+{
+    public class PackingBoy
+    {
+        private List<PackingLot> packingLots;
+
+        public PackingBoy(List<PackingLot> packingLots)
+        {
+            this.packingLots = packingLots;
+        }
+
+        public PackingBoy(int packingLotsCount)
+        {
+            packingLots = new List<PackingLot>();
+            for (int i = 0; i < packingLotsCount; i++)
+            {
+                packingLots.Add(new PackingLot());
+            }
+        }
+
+        public Ticket PackingCar(string carNum)
+        {
+            Ticket ticket = null;
+            packingLots.ForEach(p =>
+            {
+                var t = p.CarIn(carNum);
+                if (t != null)
+                {
+                    ticket = t;
+                }
+            });
+            return ticket;
+        }
+    }
+}
